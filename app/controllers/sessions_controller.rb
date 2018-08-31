48b15503a94f0eb3ff66f9958842a662374c0023
Jamie Brown <jmbrowncow@gmail.com>
2018-08-31 20:49:57 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:name]=params[:name]
-    redirect_to '/'
+    if params[:name]
+      session[:name]=params[:name]
+      redirect_to '/'
+    else
+      render :new
+    end
   end
 
   def destroy
